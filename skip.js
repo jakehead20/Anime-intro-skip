861fa913b23839fa9a109a19487b3bd66ba01143
jakehead20 <jakehead20@gmail.com>
2018-07-22 11:25:36 -0700

    Changed require. Added vidstreaming.io

--- skip.js
+++ skip.js
@@ -4,12 +4,13 @@
 // @namespace http://tampermonkey.net/
 // @grant GM_setValue
 // @grant GM_getValue
-// @require http://userscripts-mirror.org/scripts/source/107941.user.js
+// @require https://greasyfork.org/scripts/370520-super-gm-set-and-get/code/Super%20GM%20set%20and%20get.js?version=614650
 // @include *rapidvideo.com*
 // @include *mp4upload.com*
 // @include *streamango.com*
+// @include *vidstreaming.io*
 // @noframes
-// @version 1.0.5
+// @version 1.0.0
 // ==/UserScript==
 
 $(document).ready(function(){
@@ -17,17 +18,26 @@ $(document).ready(function(){
     let key = GM_SuperValue.get("keyCode", 76); // Key used to skip time
     let setkey = GM_SuperValue.get("settingkeyCode", 189); // Key used to change settings
     let changeSettings = function(){
+        let timehold = time;
+        let keyhold = key;
         time = prompt("How many seconds should we skip?", time);
+        if(!time){
+            time = timehold;
+        }
         GM_SuperValue.set("skipTime", time);
         key = prompt("Keycode for skipping time (go to keycode.info for keycodes).", key);
+        if(!key){
+            key = keyhold;
+        }
         GM_SuperValue.set("keyCode", key);
     };
+    let url = window.location.href;
     if (typeof jwplayer === 'function'){
         window.addEventListener('keydown', function(e){
-            if (e.which === key){
-                jwplayer().seek(jwplayer().getPosition()+time);
+            if (e.which == key){
+                jwplayer().seek(jwplayer().getPosition()+parseInt(time));
             }
-            if (e.which === setkey){
+            if (e.which == setkey){
                 changeSettings();
             }
         });
@@ -39,7 +49,7 @@ $(document).ready(function(){
                 window.addEventListener('keydown', function(e){
                     if (e.which == key){
                         let player = vids[Object.keys(vids)[0]];
-                        player.currentTime(parseInt(player.currentTime())+time);
+                        player.currentTime(player.currentTime()+parseInt(time));
                     }else
                     if (e.which == setkey){
                         changeSettings();
