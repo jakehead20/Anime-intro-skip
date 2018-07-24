6ee0e51f9d732cbb67bbc28a6c38ac6ed8397ed5
jakehead20 <jakehead20@gmail.com>
2018-07-23 17:10:26 -0700

    Added kissanime
    Whoop

--- skip.js
+++ skip.js
@@ -5,15 +5,16 @@
 // @grant GM_setValue
 // @grant GM_getValue
 // @require https://greasyfork.org/scripts/370520-super-gm-set-and-get/code/Super%20GM%20set%20and%20get.js?version=614650
+// @require http://code.jquery.com/jquery-1.7.1.min.js
 // @include *rapidvideo.com*
 // @include *mp4upload.com*
 // @include *streamango.com*
 // @include *vidstreaming.io*
-// @noframes
+// @include *kissanime.ru/Anime/*
 // @version 1.0.0
 // ==/UserScript==
-
 $(document).ready(function(){
+    console.log(window.self === window.top);
     let time = GM_SuperValue.get("skipTime", 85); // 1:25 just in case you're a little bit farther away from your keyboard :)
     let key = GM_SuperValue.get("keyCode", 76); // Key used to skip time
     let setkey = GM_SuperValue.get("settingkeyCode", 189); // Key used to change settings
@@ -33,29 +34,31 @@ $(document).ready(function(){
     };
     let url = window.location.href;
     if (typeof jwplayer === 'function'){
-        window.addEventListener('keydown', function(e){
+        let callback = function(e){
             if (e.which == key){
                 jwplayer().seek(jwplayer().getPosition()+parseInt(time));
             }
             if (e.which == setkey){
                 changeSettings();
             }
-        });
+        };
+        window.addEventListener('keydown', callback);
     }else{
         if (typeof videojs === 'function') {
             let vids = videojs.getPlayers();
             let player = vids[Object.keys(vids)[0]];
+            let callback = function(e){
+                if (e.which == key){
+                    let player = vids[Object.keys(vids)[0]];
+                    player.currentTime(player.currentTime()+parseInt(time));
+                }else
+                if (e.which == setkey){
+                    changeSettings();
+                }
+            };
             player.ready(function(){
-                window.addEventListener('keydown', function(e){
-                    if (e.which == key){
-                        let player = vids[Object.keys(vids)[0]];
-                        player.currentTime(player.currentTime()+parseInt(time));
-                    }else
-                    if (e.which == setkey){
-                        changeSettings();
-                    }
-                });
-            })
+                window.addEventListener('keydown', callback);
+            });
         }
     }
 });
