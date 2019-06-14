9bbdfcfde17e059819cdf184988b3df72bab8835
jakehead20 <jakehead20@gmail.com>
2019-06-13 18:44:02 -0700

    Copy functionality
    Added function to copy the video source's URL, meaning you can copy a link that sends you to the video file.

--- skip.user.js
+++ skip.user.js
@@ -1,13 +1,41 @@
 // ==UserScript==
 // @name         AIS
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
-// @description  Period to skip forward, comma to skip backward (cause why not), slash to edit keybinds and skip time. Works only on HTML5 videos for now.
+// @version      1.0.5
+// @description  Period to skip forward, comma to skip backward (cause why not), slash to edit keybinds and skip time, U to video source's URL. Works only on HTML5 videos for now.
 // @author       jaek#9509
 // @match        https://twist.moe/a/*/*
 // @grant        GM.getValue
 // @grant        GM.setValue
 // ==/UserScript==
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Fallback: Copying text command was ' + msg);
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
+  }
+
+  document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(function() {
+    console.log('Async: Copying to clipboard was successful!');
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
+  });
+}
 
 (async function() {
     'use strict;'
@@ -17,7 +45,8 @@
     var skipTime = await get("skipTime", 85);
     var forwardKey = await get("forwardKey", 190);
     var backKey = await get("backKey", 188);
-    var editKey = 191
+    var copyKey = 85;
+    var editKey = 191;
     document.addEventListener("keydown", function(e){
         var vid = document.getElementsByTagName("video")[0];
         switch(e.keyCode){
@@ -35,6 +64,8 @@
                 set("forwardKey", forwardKey);
                 set("backKey", backKey);
                 break;
+            case copyKey:
+                copyTextToClipboard(vid.src);
             default: return;
         }
     e.preventDefault()
